a21bc0f332c2db3583d09c373c8f75e5d54cca56
lisaj4f <Lisa@J4F.be>
2021-12-14 16:42:57 +0100

    a

---
+++ tsx/KM7Zc+.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="KM7Zc+" tilewidth="32" tileheight="32" tilecount="72" columns="8">
+ <image source="../../../Downloads/KM7Zc+.png" width="284" height="300"/>
+</tileset>
